e332a73fc1268934024d9ebc5c130587d0b9e9ec
Joaquin Pareja <joaq.pareja@gmail.com>
2023-03-19 23:43:41 -0300

    leaving the deconstruct just as deconstruct

--- cypress/e2e/Geant/deConstrucProduct/deConstructProducts.cy.ts
+++ cypress/e2e/Geant/deConstrucProduct/deConstructProducts.cy.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 import {homePageGeant} from '../../../support/Pages/SuperMarkets'
 
-const datatest = process.env.jsonFile;
+
 describe('Get the page information',()=>{
 
     it('Store the page information', ()=>{
@@ -14,16 +14,7 @@ describe('Get the page information',()=>{
         homePageGeant.pushIntoCoffeeListArray();
         homePageGeant.writefile();
      })
-     it('check json', ()=>{
-        cy.readFile(datatest).then((str)=>{
-            cy.wrap(str).pause()
-            cy.wrap(str[0]).pause()
-            cy.wrap(str[0][1]).pause()
-            cy.wrap(str[1]).pause()
 
-            cy.wrap(str[0].ProductBrand)
-        })
-     })
 })
 
 
