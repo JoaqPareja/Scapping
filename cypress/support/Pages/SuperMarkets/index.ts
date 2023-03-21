01900aea3c92130f77691d910e02b6a67d521f38
Joaquin Pareja <joaq.pareja@gmail.com>
2023-03-21 10:30:57 -0300

    test

--- cypress/support/Pages/SuperMarkets/index.ts
+++ cypress/support/Pages/SuperMarkets/index.ts
@@ -1,6 +1,6 @@
 
-import HomePageDevoto from "./Devoto/homepage"
+import HomePageDevoto from "./Dvto/homepage"
 export const homePageDevoto = new HomePageDevoto();
 
-import HomePageGeant from "./Geant/homepage";
+import HomePageGeant from "./Gnt/homepage";
 export const homePageGeant = new HomePageGeant();
