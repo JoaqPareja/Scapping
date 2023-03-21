4db232a12f41aa07e73395083aa603eb4abae04e
Joaquin Pareja <joaq.pareja@gmail.com>
2023-03-21 12:22:25 -0300

    adding assertions

--- cypress/e2e/Gnt/reConstrucProduct/reConstrucProduct.cy.ts
+++ cypress/e2e/Gnt/reConstrucProduct/reConstrucProduct.cy.ts
@@ -18,9 +18,17 @@ let elements:unknown;
     it('Convert array',()=>{   
             cy.reCreateProduct(productBrand, elements, titleProduct, priceProduct, arrayCoffeeListArray)
             cy.wrap(arrayCoffeeListArray).should('not.be.empty')
+            // cy.wrap(arrayCoffeeListArray[1][1]).should('have.string')
         })
-    it('Check positions of Array', ()=>{
+    it('Check positions of Array and push into new Json', ()=>{
         cy.writeFile(datatestGeant, arrayCoffeeListArray)
+         cy.wrap(arrayCoffeeListArray[0][0]).should('not.be.empty');
+         cy.wrap(arrayCoffeeListArray[0][0].Marca).should('not.be.empty');
+         cy.wrap(arrayCoffeeListArray[0][0].Marca).should('not.contain', '1234567890!@#$%^&*()_+=')
+         cy.wrap(arrayCoffeeListArray[0][0].Descripcion).should('not.be.empty');
+         cy.wrap(arrayCoffeeListArray[0][0].Precio).should('not.be.empty');
+         cy.wrap(arrayCoffeeListArray[0][0].Precio).should('not.contain', 'abcdefghijklmnñopqrstuvwxyz')
+        //  cy.wrap(arrayCoffeeListArray[0][0].Precio).should('not.have.value', 'text');
         cy.readFile(datatestGeant).then((str)=>{
             cy.wrap(str).should('not.be.empty')
         })
