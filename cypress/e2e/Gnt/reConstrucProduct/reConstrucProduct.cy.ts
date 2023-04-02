ebe4c868cc503d7b94a63ca95754d217a84965d2
Joaquin Pareja <joaq.pareja@gmail.com>
2023-04-02 10:18:52 -0300

    forcing to be worng

--- cypress/e2e/Gnt/reConstrucProduct/reConstrucProduct.cy.ts
+++ cypress/e2e/Gnt/reConstrucProduct/reConstrucProduct.cy.ts
@@ -14,15 +14,22 @@ describe('Re convert products',()=>{
             reConstructCoffePage.createArrNormalAndSantanderPrices();
         })
         it('Check normal and santander prices',()=>{
+            // console.log(reConstructCoffePage.arrayOfPrecios)
             cy.wrap(reConstructCoffePage.arrayOfPrecios).each((txt:any)=>{
+                // cy.wrap(txt[0]).pause();
+                // cy.wrap(txt).pause()
+                //  cy.wrap(txt.Marca).pause()
+                //  cy.wrap(txt.Descripcion).pause()
+                //  cy.wrap(txt.NormalPrice).pause()
+                //  cy.wrap(txt.SantanderPrice).pause()
                 cy.wrap(txt).should('not.be.empty');
-                cy.wrap(txt.Marca).should('not.be.empty');
-                cy.wrap(txt.Marca).should('not.contain', '1234567890!@#$%^&*()_+=')
-                cy.wrap(txt.Descripcion).should('not.be.empty');
-                cy.wrap(txt.NormalPrice).should('not.be.empty');
-                cy.wrap(txt.NormalPrice).should('not.contain', 'abcdefghijklmnñopqrstuvwxyz')
-                cy.wrap(txt.SantanderPrice).should('not.be.empty');
-                cy.wrap(txt.SantanderPrice).should('not.contain', 'abcdefghijklmnñopqrstuvwxyz')
+                cy.wrap(txt[0].Marca).should('not.be.empty');
+                cy.wrap(txt[0].Marca).should('not.contain', '1234567890!@#$%^&*()_+=')
+                cy.wrap(txt[0].Descripcion).should('not.be.empty');
+                cy.wrap(txt[0].NormalPrice).should('not.be.empty');
+                cy.wrap(txt[0].NormalPrice).should('not.contain', 'abcdefghijklmnñopqrstuvwxyz')
+                cy.wrap(txt[0].SantanderPrice).should('not.be.empty');
+                cy.wrap(txt[0].SantanderPrice).should('not.contain', 'abcdefghijklmnñopqrstuvwxyz')
             })
         })
         it('Get the USD prices', ()=>{
@@ -52,23 +59,46 @@ describe('Re convert products',()=>{
                 cy.wrap(txt.NormalPrice).should('not.contain', 'abcdefghijklmnñopqrstuvwxyz')
             })
         })
+        
         it('Concat both arrays and check them', ()=>{
+           
             reConstructCoffePage.unifyArrays()
-            cy.wrap(reConstructCoffePage.newArrayCoffeeListArray).each((txt:any)=>{
-                cy.wrap(txt).should('not.be.empty');
-                cy.wrap(txt.Marca).should('not.be.empty');
+            cy.wrap(reConstructCoffePage.newArrayCoffeeListArray).each((txt:any, index )=>{
+                cy.wrap(txt).should('not.to.be.null');
+                // console.log(txt, 'txt')
+            
+                // cy.wrap(txt).pause();
+                // cy.wrap(txt[0]).pause();
+                // cy.wrap(txt[0].Marca).should('not.to.be.null');
                 cy.wrap(txt.Marca).should('not.contain', '1234567890!@#$%^&*()_+=')
-                cy.wrap(txt.Descripcion).should('not.be.empty');
-                cy.wrap(txt.NormalPrice).should('not.be.empty');
-                cy.wrap(txt.NormalPrice).should('not.contain', 'abcdefghijklmnñopqrstuvwxyz')
-                if(txt.SantanderPrice){
-                    cy.log('santander price exist')
-                    cy.wrap(txt.SantanderPrice).should('not.be.empty');
-                    cy.wrap(txt.SantanderPrice).should('not.contain', 'abcdefghijklmnñopqrstuvwxyz')
+                cy.wrap(txt.Descripcion).should('not.to.be.null');
+                // console.log(txt.NormalPrice)
+                // cy.log(txt.NormalPrice).pause()
+                try {
+                    expect(txt.NormalPrice).to.not.be.null;
+                    // expect(txt.NormalPrice).to.not.contain.value('abcdefghijklmnñopqrstuvwxyz')
+                } catch (error) {
+                    cy.log(error).pause();
+                    console.log(error)
                 }
+              
+                
+                // cy.wrap(txt.NormalPrice).should('not.be', undefined);
+                // cy.wrap(txt.NormalPrice).should('not.contain.value', 'abcdefghijklmnñopqrstuvwxyz')
+                try{
+                    if(txt.SantanderPrice){
+                        cy.log('santander price exist')
+                        cy.wrap(txt.SantanderPrice).should('not.to.be.null');
+                        cy.wrap(txt.SantanderPrice).should('not.contain.value', 'abcdefghijklmnñopqrstuvwxyz')
+                    }
+                }
+                catch(err){
+                    cy.log(err);
+                }
+                
             })
         })
     it('push into new Json',()=>{
-        reConstructCoffePage.pushJson('not.be.empty')
+        reConstructCoffePage.pushJson('not.to.be.null')
     })
 })
--- cypress/support/Pages/SuperMarkets/Gnt/coffePage/coffepPagePostProducts.ts
+++ cypress/support/Pages/SuperMarkets/Gnt/coffePage/coffepPagePostProducts.ts
@@ -12,7 +12,7 @@ export default class ReConstructCoffePage {
     elements:any; 
     newArrayCoffeeListArray:Array<any> =[];
     str:any;
-     element:any
+    element:any
     readfile(Assertion1:string, Assertion2:string, Assertion3:string, Assertion4:string){
         cy.readFile(this.datatestRaw).then((str)=>{
             cy.wrap(str).should(Assertion1)
@@ -27,24 +27,37 @@ export default class ReConstructCoffePage {
     createArrNormalAndSantanderPrices(){
         for (let index = 0; index < this.arrayCoffeeListArray.length; index++) {
             this.element = this.arrayCoffeeListArray[index];
-            if( this.element[0].Precio.charAt(3) &&  this.element[0].Precio.charAt(3) === '$'  ||  this.element[0].Precio.charAt(4) === '$' ){
+            if(this.element[0].Precio.charAt(3) && this.element[0].Precio.charAt(3) === '$'  ||  this.element[0].Precio.charAt(4) === '$'||  this.element[0].Precio.charAt(6) === '$'){
                 this.elements =  this.element[0].Precio.split('$')
-                cy.wrap( this.elements).then((txt)=>{
-                    this.arrayOfPrecios.push({Marca: this.element[0].Marca, Descripcion:  this.element[0].Descripcion, NormalPrice:'$'+txt[1], SantanderPrice: '$'+txt[2]})
-                })
+                let marca:Array<any> = [this.element[0].Marca]
+                let descripcion:Array<string> = [this.element[0].Descripcion]
+                cy.wrap( this.elements).then((txt:unknown)=>{
+                    let tempElement:any = marca.map(marca => new Array({ Marca: marca, Descripcion: descripcion[0], NormalPrice:'$'+txt[1], SantanderPrice: '$'+txt[2]}))
+                    // let str  = tempElement[0].toString();
+                    // cy.wrap(str).pause()
+                    // console.log(str)
+                   
+                    this.arrayOfPrecios.push(tempElement[0]);
+                    // this.arrayOfPrecios.toString()
+                    // cy.log(this.arrayOfPrecios.toString())
+                    // cy.wrap(this.arrayOfPrecios).pause();
+                    // cy.wrap(this.arrayOfPrecios[0]).pause();
+                    // cy.wrap(this.arrayOfPrecios[0].Marca).pause();
+                })              
             }     
         }
+
     }
     createUSDPrices(){
         for (let index = 0; index < this.arrayCoffeeListArray.length; index++) {
             this.element = this.arrayCoffeeListArray[index];
             if(this.element[0].Precio.charAt(7) == 'U'){
                 this.elements = this.element[0].Precio.split('U');
-                cy.wrap(this.elements).then((txt)=>{
+                cy.wrap(this.elements).then((txt:unknown)=>{
                     this.arrayOfUSDPrecios.push({Marca:this.element[0].Marca, Descripcion: this.element[0].Descripcion, NormalPrice:'U'+txt[1], SantanderPrice:'U'+txt[2]})
             })
         }
-    }
+     }
     }
     createNormalPrices(){
         for (let index = 0; index < this.arrayCoffeeListArray.length; index++) {
@@ -55,13 +68,20 @@ export default class ReConstructCoffePage {
         }
     }
     unifyArrays(){
-        this.newArrayCoffeeListArray =  this.newArrayCoffeeListArray.concat(this.normalArr,this.arrayOfPrecios,this.arrayOfUSDPrecios)
-
+        this.newArrayCoffeeListArray =  this.newArrayCoffeeListArray.concat(this.normalArr, JSON.stringify(this.arrayOfPrecios),this.arrayOfUSDPrecios)
     }
-    pushJson(Asseertion:string){
+    pushJson(Asseertion1:string){
         cy.writeFile(this.datatestGeant, this.newArrayCoffeeListArray)
-           cy.readFile(this.datatestGeant).then((str)=>{
-           cy.wrap(str).should(Asseertion)         
+           cy.readFile(this.datatestGeant).each((str:any)=>{
+           cy.wrap(str).should(Asseertion1) 
+        //    cy.wrap(str.Marca).pause();
+           cy.wrap(str.Marca).should(Asseertion1)          
+           cy.wrap(str.Descripcion).should(Asseertion1) 
+           cy.wrap(str.NormalPrice).should(Asseertion1) 
+           if(str.SantanderPrice){
+            cy.log('santander price exist')
+            cy.wrap(str.SantanderPrice).should(Asseertion1);
+        }
        })
     }
 }
