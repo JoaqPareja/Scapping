73db85b9100256d7726aebdcd90684231fcab4e9
Joaquin Pareja <joaq.pareja@gmail.com>
2023-04-02 11:11:03 -0300

    changes in see all products

--- cypress/support/Pages/SuperMarkets/Gnt/homepage.ts
+++ cypress/support/Pages/SuperMarkets/Gnt/homepage.ts
@@ -1,18 +1,11 @@
 import  Urls  from './urls';
 export default class HomePageGeant extends Urls {
-    productBrand:Array<unknown> =[];
-    titleProduct:Array<unknown> =[]; 
-    priceProduct:Array<unknown> =[]; 
-    arrayCoffeeListArray:Array<unknown> =[]; ;
-    datatestRawGeant:string = 'cypress/fixtures/datatestRaw.json'
-    tryElement:unknown; 
     constructor(){
         super();  
     }
     elementsHPGnt={
-        eachBox:()=> cy.get('div.styles__ProductItem-tbq658-3 > div:nth-child(3)'),
         searchInput:()=> cy.get('.styles__Input-sc-157ulfq-1 > input:nth-child(1)'),
-        seeAllProducts: ()=> cy.get('.styles__ResultsContainer-sc-1skad6n-3 > .jss3'),
+        seeAllProducts: ()=> cy.get('.styles__SeeAllResults-sc-1skad6n-11'),
     }
     clickSeeAllProducts(){
         this.elementsHPGnt.seeAllProducts().click();
@@ -20,25 +13,6 @@ export default class HomePageGeant extends Urls {
     typeSearchInput($el: string){
         this.elementsHPGnt.searchInput().type($el)
     }
-    storeProducts(){
-        this.elementsHPGnt.eachBox().each(($el)=>{
-            cy.wrap($el).within(($element)=>{
-                cy.wrap($element).find('a > p').then((response: { text: () => string;})=>{
-                    this.productBrand.push(response.text())       
-            })
-            cy.wrap($element).find('a > h2').then((response: { text: () => string;})=>{
-                this.titleProduct.push( response.text())
-            })
-            cy.wrap($element).find('div > p').then((response: { text: () => string;})=>{
-                this.priceProduct.push(response.text())
-            })  
-        })
-    });
-            return  this.arrayCoffeeListArray.push( this.productBrand, this.titleProduct, this.priceProduct)
-}   
-        writefile(){
-            cy.writeFile(this.datatestRawGeant, this.arrayCoffeeListArray)
 
-        }
     }
     
