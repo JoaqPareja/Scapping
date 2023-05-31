0a49d506517001ad7b873d6c930d2048b346804b
Joaquin Pareja <joaq.pareja@gmail.com>
2023-05-30 22:15:24 -0300

    test

--- cypress/support/Pages/SuperMarkets/index.ts
+++ cypress/support/Pages/SuperMarkets/index.ts
@@ -1,10 +1,10 @@
 
 
 
-import DvtHomePage from "./dvt/homepage"
+import DvtHomePage from "../SuperMarkets/dvt/homepage"
 export const dvtHomePage = new DvtHomePage();
-
-import DvtDeConstructCoffeePage from "./dvt/coffePage/1.getPostProducts"
+// dvt/ /dvt/coffePage/1.getPostProducts
+import DvtDeConstructCoffeePage from "../SuperMarkets/dvt/coffePage/1.getPostProducts"
 export const dvtDeConstructCoffePage = new DvtDeConstructCoffeePage();
 
 //Create Basic Global Elements
@@ -15,19 +15,23 @@ export const dvtDeConstructCoffePage = new DvtDeConstructCoffeePage();
 //passing the argument needed From the class 
 
 //Basic elements
-import GntGlobalElements from "./gnt/001.pageElements/005.gntGlobalElements";
+///gnt/001.pageElements/005.gntGlobalElements";
+import GntGlobalElements from "../SuperMarkets/gnt/001.pageElements/005.gntGlobalElements";
 export const gntGlobalElements = new GntGlobalElements();
 
 //Creating specific object from Specific Coffe elements class
-import CoffePage from "./gnt/002.productsPages/coffeePage/coffePage"
+//./gnt/002.productsPages/coffeePage/coffePage"
+import CoffePage from "../SuperMarkets/gnt/002.productsPages/coffeePage/coffePage"
 export const gntDeConstructCoffePage = new CoffePage();
 
 //Creating specific object from Specific Atun elements class
-import AtunPage from "./gnt/002.productsPages/atunPage/atunPage"
+//gnt/002.productsPages/atunPage/atunPage"
+import AtunPage from "../SuperMarkets/gnt/002.productsPages/atunPage/atunPage"
 export const gntDeConstructAtunPage = new AtunPage();
 
 //Creating specific object from Specific Wine elements class
-import WinePage from "./gnt/002.productsPages/winePage/winePage"
+//./gnt/002.productsPages/winePage/winePage
+import WinePage from "../SuperMarkets/gnt/002.productsPages/winePage/winePage"
 export const gntDeConstructWinePage = new WinePage();
 
 import GntStoreProducts from "../SuperMarkets/gnt/003.gntStoreProducts/GntStoreProducts";
